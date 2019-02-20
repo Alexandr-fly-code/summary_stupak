9d4ce267e447f9b8f2f1c90fc6c7e6bb4d232598
alexandr-fly-code <alexandr17world@gmail.com>
2019-02-20 18:36:04 +0200

    Updates

---
+++ precache-manifest.56b02661871da7452273682055f7952a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2852c37b8b88d6f98a11728970fdbd0",
+    "url": "/summary_stupak/static/media/headphones.d2852c37.svg"
+  },
+  {
+    "revision": "90b5c3231e9b6743ffc2",
+    "url": "/summary_stupak/static/css/main.a5c8a1e6.chunk.css"
+  },
+  {
+    "revision": "9b76948911e9c197b00f",
+    "url": "/summary_stupak/static/js/1.9b769489.chunk.js"
+  },
+  {
+    "revision": "3c8669a8c96a62bad0bb",
+    "url": "/summary_stupak/static/js/runtime~main.3c8669a8.js"
+  },
+  {
+    "revision": "506975e6695a02a9ff2a275a49c05128",
+    "url": "/summary_stupak/static/media/photo-camera.506975e6.svg"
+  },
+  {
+    "revision": "90b5c3231e9b6743ffc2",
+    "url": "/summary_stupak/static/js/main.90b5c323.chunk.js"
+  },
+  {
+    "revision": "d38694f809859add39e67cffc90c3006",
+    "url": "/summary_stupak/static/media/html.d38694f8.svg"
+  },
+  {
+    "revision": "e19bdd2ef9c6051c1119750d96b70b63",
+    "url": "/summary_stupak/static/media/tennis-ball.e19bdd2e.svg"
+  },
+  {
+    "revision": "ae902787818c651ef316922774e094b6",
+    "url": "/summary_stupak/static/media/pin-pong.ae902787.svg"
+  },
+  {
+    "revision": "90a9536ecfaea7d513d9e533ba54a676",
+    "url": "/summary_stupak/static/media/avatar.90a9536e.png"
+  },
+  {
+    "revision": "213701a04601fdecaf3d05fe15303465",
+    "url": "/summary_stupak/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/summary_stupak/precache-manifest.56b02661871da7452273682055f7952a.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/summary_stupak/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
